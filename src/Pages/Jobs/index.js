6f3369e35f7ea82b696e49c82820cfdc016d3fe4
estherlardze <estherlardze8@gmail.com>
2023-10-20 11:56:09 +0000

    created jobs data

---
+++ src/Pages/Jobs/index.js
@@ -0,0 +1,122 @@
+export const jobs = [
+    { id:1,
+      companyName: "Google AI",
+      position: "Software Engineer",
+      numberOfVacancies: 1,
+      location: "Mountain View, CA",
+      dayPosted: "2 days ago",
+      salaryRange: {
+        min: 120000,
+        max: 150000
+      },
+      fullTimeOrRemote: "Hybrid"
+    },
+    { id:2,
+      companyName: "Meta",
+      position: "Data Scientist",
+      numberOfVacancies: 2,
+      location: "New York, NY",
+      dayPosted: "1 day ago",
+      salaryRange: {
+        min: 130000,
+        max: 160000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:3,
+      companyName: "Amazon",
+      position: "Product Manager",
+      numberOfVacancies: 3,
+      location: "Seattle, WA",
+      dayPosted: "3days ago",
+      salaryRange: {
+        min: 140000,
+        max: 170000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:4,
+      companyName: "Microsoft",
+      position: "DevOps Engineer",
+      numberOfVacancies: 1,
+      location: "Redmond, WA",
+      dayPosted: "2 days ago",
+      salaryRange: {
+        min: 110000,
+        max: 140000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:5,
+      companyName: "Apple",
+      position: "iOS Engineer",
+      numberOfVacancies: 1,
+      location: "Cupertino, CA",
+      dayPosted: "1 week ago",
+      salaryRange: {
+        min: 125000,
+        max: 155000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:6,
+      companyName: "Netflix",
+      position: "Backend Engineer",
+      numberOfVacancies: 2,
+      location: "Los Gatos, CA",
+      dayPosted: "3 days ago",
+      salaryRange: {
+        min: 135000,
+        max: 165000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    { id:7,
+      companyName: "Airbnb",
+      position: "Frontend Engineer",
+      numberOfVacancies: 1,
+      location: "San Francisco, CA",
+      dayPosted: "4 days ago",
+      salaryRange: {
+        min: 120000,
+        max: 150000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:8,
+      companyName: "Spotify",
+      position: "Machine Learning Engineer",
+      numberOfVacancies: 2,
+      location: "Stockholm, Sweden",
+      dayPosted: "1 day ago",
+      salaryRange: {
+        min: 130000,
+        max: 160000
+      },
+      fullTimeOrRemote: "Hybrid"
+    },
+    { id:9,
+      companyName: "Stripe",
+      position: "Software Engineer",
+      numberOfVacancies: 3,
+      location: "Dublin, Ireland",
+      dayPosted: "5 days ago",
+      salaryRange: {
+        min: 140000,
+        max: 170000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:10,
+      companyName: "Twitter",
+      position: "Site Reliability Engineer",
+      numberOfVacancies: 1,
+      location: "Singapore",
+      dayPosted: "3 days ago",
+      salaryRange: {
+        min: 110000,
+        max: 140000
+      },
+      fullTimeOrRemote: "Full time"
+    }
+]  
--- src/components/Jobs/JobsCardTwo.jsx
+++ src/components/Jobs/JobsCardTwo.jsx
@@ -1,8 +1,13 @@
 
 const JobsCardTwo = () => {
+
   return (
-    <div>
-      
+    <div className="">
+      <div className="bg-blue items-center flex rounded-tr-xl rounded-tl-xl sm:h-20  p-4 space-x-3 flex-wrap">
+       <h3 className="capitalize sm:text-3xl font-bold text-white mx-8">Featured Companies</h3>
+      </div>
+      <div className="px-12 py-6 space-y-4 bg-blue/10 rounded-br-xl rounded-bl-xl pb-12">
+    </div>
     </div>
   )
 }
