e777d94a092c8ce536c7f7f718af13cbc012e584
estherlardze <estherlardze8@gmail.com>
2023-10-20 16:44:07 +0000

    working on job detail

--- src/Pages/JobDetail/JobDetail.jsx
+++ src/Pages/JobDetail/JobDetail.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { jobs } from '.'
+import {GrLocation} from 'react-icons/gr'
+import {AiOutlineCalendar} from 'react-icons/ai'
 
 const JobDetail = () => {
   return (
-    <div>
-      JobDetail
-    </div>
+    <section className=''>
+       <div className='bg-blue/5 py-[50px] px-[10%]'>
+         {jobs.slice(0, 1).map((job) => (
+          <article>
+            <div>
+              <div className='flex gap-3'>
+                <img src={job.logo} alt="logo" className='bg-black/40 p-3 rounded-md'/>
+                 <div>
+                  <h1 className='font-bold text-4xl'>{job.position}</h1> 
+                  <div>
+                    <p><GrLocation/> {job.location}</p>
+                    <p><AiOutlineCalendar/>{job.dayPosted}</p>
+                  </div>
+                 </div>
+
+              </div>
+            </div>
+
+          </article>
+         ))}
+       </div>
+    </section>
   )
 }
 
---
+++ src/Pages/JobDetail/index.js
@@ -0,0 +1,143 @@
+import {seniorUIUXDesigner} from '../../assets/index'
+
+export const jobs = [
+    { id:1,
+      logo: seniorUIUXDesigner,
+      companyName: "Google AI",
+      position: "Database Administrator",
+      numberOfVacancies: 1,
+      location: "Mountain View, CA",
+      dayPosted: "2 days ago",
+      tagName: "arts",
+      salaryRange: {
+        min: 120000,
+        max: 150000
+      },
+      fullTimeOrRemote: "Hybrid",
+      description: "We are seeking an experienced Database Administrator to manage and maintain our company's databases. You will be responsible for ensuring the performance, integrity, and security of the databases, as well as planning and implementing backup and recovery strategies.",
+      requirement: "Bachelor's degree in Computer Science, Information Technology, or a related field, proven work experience as a Database Administrator, in-depth understanding of database structure principles, experience with database standards and end-user applications, familiarity with database design, documentation, and coding, and proficiency in data management and data processing languages such as SQL.",
+      responsibilities: "Monitor database performance, implement changes and apply new patches and versions when required, ensure data security and troubleshoot any issues on behalf of users, create and maintain database documentation, including data standards, procedures, and definitions, and perform regular backups to ensure data preservation."
+
+    },
+    { id:2,
+      companyName: "Meta",
+      position: "Organist",
+      numberOfVacancies: 2,
+      location: "New York, NY",
+      dayPosted: "1 day ago",
+      tagName: "music",
+      salaryRange: {
+        min: 130000,
+        max: 160000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:3,
+      companyName: "Amazon",
+      position: "Product Manager",
+      numberOfVacancies: 3,
+      location: "Seattle, WA",
+      dayPosted: "3days ago",
+      tagName: "health",
+      salaryRange: {
+        min: 140000,
+        max: 170000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:4,
+      companyName: "Microsoft",
+      position: "DevOps Engineer",
+      numberOfVacancies: 1,
+      location: "Redmond, WA",
+      dayPosted: "2 days ago",
+      tagName: "finance",
+      salaryRange: {
+        min: 110000,
+        max: 140000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:5,
+      companyName: "Apple",
+      position: "iOS Engineer",
+      numberOfVacancies: 1,
+      location: "Cupertino, CA",
+      dayPosted: "1 week ago",
+      tagName: "marketing",
+      salaryRange: {
+        min: 125000,
+        max: 155000
+      },
+      fullTimeOrRemote: "Full time",
+      description: "We are seeking a skilled Software Engineer to join our dynamic team...",
+      requirement: "Bachelor's degree in Computer Science or a related field, proficiency in Java and Python, strong problem-solving skills.",
+      responsibilities: "Develop software applications, participate in design and code reviews, troubleshoot and resolve issues, and contribute to all phases of the development lifecycle."
+    },
+    
+    {  id:6,
+      companyName: "Netflix",
+      position: "Backend Engineer",
+      numberOfVacancies: 2,
+      location: "Los Gatos, CA",
+      dayPosted: "3 days ago",
+      tagName: "teaching",
+      salaryRange: {
+        min: 135000,
+        max: 165000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    { id:7,
+      companyName: "Airbnb",
+      position: "Frontend Engineer",
+      numberOfVacancies: 1,
+      location: "San Francisco, CA",
+      dayPosted: "4 days ago",
+      tagName: "photography",
+      salaryRange: {
+        min: 120000,
+        max: 150000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:8,
+      companyName: "Spotify",
+      position: "Machine Learning Engineer",
+      numberOfVacancies: 2,
+      location: "Stockholm, Sweden",
+      dayPosted: "1 day ago",
+      tagName: "It",
+      salaryRange: {
+        min: 130000,
+        max: 160000
+      },
+      fullTimeOrRemote: "Hybrid"
+    },
+    { id:9,
+      companyName: "Stripe",
+      position: "Software Engineer",
+      numberOfVacancies: 3,
+      location: "Dublin, Ireland",
+      dayPosted: "5 days ago",
+      tagName: "It",
+      salaryRange: {
+        min: 140000,
+        max: 170000
+      },
+      fullTimeOrRemote: "Full time"
+    },
+    {  id:10,
+      companyName: "Twitter",
+      position: "Site Reliability Engineer",
+      numberOfVacancies: 1,
+      location: "Singapore",
+      dayPosted: "3 days ago",
+      tagName: "music",
+      salaryRange: {
+        min: 110000,
+        max: 140000
+      },
+      fullTimeOrRemote: "Full time"
+    }
+]  
