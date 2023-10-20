bf5638bc59c366a06adb3bfd9ac9632cdaeb268e
estherlardze <estherlardze8@gmail.com>
2023-10-20 08:20:01 +0000

    created three pages

---
+++ src/Pages/Changelog/Changelog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Changelog = () => {
+  return (
+    <div>
+      Changelog
+    </div>
+  )
+}
+
+export default Changelog
---
+++ src/Pages/License/License.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Licence = () => {
+  return (
+    <div>Licence</div>
+  )
+}
+
+export default Licence
