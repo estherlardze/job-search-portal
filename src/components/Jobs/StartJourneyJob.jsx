f40429c72871bdfe5d3f0839f4e33baef2e4e776
Asheryram <ashertettehabotsi@gmail.com>
2023-10-21 19:00:52 +0000

    ....

--- src/components/Jobs/StartJourneyJob.jsx
+++ src/components/Jobs/StartJourneyJob.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {bluearrow} from '../../assets'
 
 const StartJourneyJob = () => {
   return (
-    <div className='py-20 border my-20 border-slate-500/20 space-y-16 px-8 overflow-hidden'>
+    <div className='py-20 border my-20 border-slate-500/20 space-y-16 px-8 overflow-hidden relative'>
+        <span className='absolute right-20 top-20 h-32 w-32 rounded-full bg-gray/10'></span>
      <p className="font-normal text-5xl text-center"> Are You Ready To Start  <br/>Your Journey?</p>
 
      <p className="text-center text-gray  font-semibold">It is a long established fact that a reader will be distracted by the real conten of a page when looking at its layout. The point of using Lorem Ipsum is.</p>
@@ -14,6 +16,9 @@ const StartJourneyJob = () => {
         Post a Free Job
     </button>
 </div>
+<img src={bluearrow} alt="bluearrow" className='rotate-180'/>
+
+
 
     </div>
   )
