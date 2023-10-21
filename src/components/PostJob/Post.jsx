0e98087b20e8ce7ee0fade85f7dc312764c48d03
estherlardze <estherlardze8@gmail.com>
2023-10-21 15:45:36 +0000

    post job done

--- src/components/PostJob/Post.jsx
+++ src/components/PostJob/Post.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Post = () => {
   return (
     <section className='bg-black/5'>
-      <section className='flex flex-col h-[50vh] md:h-[80vh] justify-center items-center text-center w-[90%] px-[5%] sm:w-[70%] sm:mx-[15%] lg:w-[50%] lg:mx-[25%]'>
+      <section className='flex flex-col h-[50vh] md:h-[80vh] justify-center items-center text-center w-[90%] px-[5%] sm:w-[70%] sm:mx-[15%] lg:w-[50%] lg:mx-[25%]'
+       data-aos="zoom-in">
         <h1 className='text-3xl sm:text-4xl md:5xl lg:text-6xl font-semibold text-black/90 '>
           Post a Job
         </h1>
