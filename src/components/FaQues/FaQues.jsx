425092d7d514348a1df78a5bd78842b293fa8291
vinny@charmswraith <vincentdanso05@gmail.com>
2023-10-21 22:38:08 +0000

    fix error

--- src/components/FaQues/FaQues.jsx
+++ src/components/FaQues/FaQues.jsx
@@ -9,9 +9,9 @@ const FaQues = () => {
         <p className="font-bold text-blue">FAQS</p>
         <p className="text-4xl font-semibold">Frequently Asked Questions</p>
       </div>
-      <div className=" flex  flex-col bg-white pb-[8rem] pt-12 lg:flex-row">
+      <div className=" mb-[-3.1rem]  flex flex-col bg-white pb-[8rem] pt-12 lg:flex-row">
         <div className="pl-8 pt-8" data-aos="fade-up">
-          <img src={faq} alt="faq" className='pr-4' />
+          <img src={faq} alt="faq" className="pr-4" />
         </div>
         <div className="w-full lg:w-1/2" data-aos="fade-up">
           <AccordionSec
